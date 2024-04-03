8378929738ae500cb343767d073fe77e40c28707
haenah <48505914+haenah@users.noreply.github.com>
2024-04-03 15:44:35 +0900

    Hammerspoon

--- main.zsh
+++ main.zsh
@@ -2,3 +2,5 @@ cp -r . $HOME
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "You need to restart your mac to apply the changes."
