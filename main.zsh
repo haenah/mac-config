8c84072b2f706dec2eed987d241914124fc4678b
haenah <48505914+haenah@users.noreply.github.com>
2025-11-05 09:39:46 +0900

    Update

--- main.zsh
+++ main.zsh
@@ -6,7 +6,7 @@ done
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 /opt/homebrew/bin/brew install nvm tig gh
-/opt/homebrew/bin/brew install --cask visual-studio-code raycast amazon-q
+/opt/homebrew/bin/brew install --cask visual-studio-code raycast amazon-q hammerspoon
 mkdir ~/.nvm
 
 open assets/Monaspace\ Neon\ Var.ttf
